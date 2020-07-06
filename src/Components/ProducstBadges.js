bc819ed063a3655d760e894890ea77a7e51a84c7
Romain KOENIG <romain.koenig@gmail.com>
2020-07-06 19:20:07 +0200

    FIX #2 : missing products

--- src/Components/ProducstBadges.js
+++ src/Components/ProducstBadges.js
@@ -11,34 +11,36 @@ class ProductsBadges extends Component {
 
   render() {
     const SpacedDiv = styled.div`margin-top: 10px;margin-bottom: 10px;`;
-    
+
     const productTypes = Object.keys(this.props.products).length;
     const productsInStock = Object.keys(this.props.products).reduce((acc, elem) =>
       acc + this.props.products[elem].quantity, 0);
     const productsToStock = Object.keys(this.props.products).reduce((acc, elem) =>
       acc + this.props.products[elem].desiredQuantity, 0);
 
-    const productsToBuy = productsToStock - productsInStock > 0 ?
-      productsToStock - productsInStock :
-      0;
+    const productsToBuy = Object.keys(this.props.products).reduce((acc, key) =>
+      acc + (this.props.products[key].quantity < this.props.products[key].desiredQuantity ?
+        this.props.products[key].desiredQuantity - this.props.products[key].quantity :
+        0)
+      , 0);
 
     return (
 
       <SpacedDiv>
-        
-          <span>Types de produits&nbsp;
+
+        <span>Types de produits&nbsp;
           <Badge pill variant="light">{productTypes}</Badge>
-          </span>
-          <span> - Produits en stock&nbsp;
+        </span>
+        <span> - Produits en stock&nbsp;
           <Badge pill variant="light">{productsInStock}</Badge>
-          </span>
-          <span> - Produits A conserver&nbsp; 
+        </span>
+        <span> - Produits A conserver&nbsp;
           <Badge pill variant="light">{productsToStock}</Badge>
-          </span>
-          <span> - Produits manquants&nbsp;
+        </span>
+        <span> - Produits manquants&nbsp;
           <Badge pill variant="light">{productsToBuy}</Badge>
-          </span>
-        
+        </span>
+
       </SpacedDiv>
 
 
