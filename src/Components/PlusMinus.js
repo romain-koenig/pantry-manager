b33aa3ec55efa260c9251f896859128eb81fa87f
Marie Gandrille <mgandrille@yahoo.fr>
2020-07-09 16:33:08 +0200

    Modification du design des bouton +/- (espacements)

--- src/Components/PlusMinus.js
+++ src/Components/PlusMinus.js
@@ -7,19 +7,22 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 const PlusMinus = (props) => {
   return (
     <ButtonGroup
-      className="btn-sm"
+      className="btn-sm border px-0"
+      size="sm"
     >
       <Button
         onClick={() => props.plus(props.id)}
-        variant="light"
+        variant="transparent"
         size="sm"
+        className="px-1"
         >
         <span role="img" aria-label="+">➕</span>
       </Button>
       <Button
         onClick={() => props.minus(props.id)}
-        variant="light"
+        variant="transparent"
         size="sm"
+        className="px-1"
         >
         <span role="img" aria-label="-">➖</span>
       </Button>
